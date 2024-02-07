4d18c1b03c3eab77cfcf56eac24f4901e6179c7e
Rodrigo <rodrigosennasantos@hotmail.com>
2024-02-07 20:23:42 -0300

    Configurando DB

--- src/main/java/com/rss/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/rss/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -1,5 +1,6 @@
 package com.rss.certification_nlw.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +11,27 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @JoinColumn(name = "student_id")
     private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grate;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
     List<AnswersCertificationsEntity> answersCertificationsEntity;
 }
--- src/main/java/com/rss/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rss/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,5 +1,6 @@
 package com.rss.certification_nlw.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,16 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "studentEntity")
     private List<CertificationStudentEntity> certificationStudentEntity;
 }
